982ed40bb1bda626752d56efa32ac6d928f58af0
alainas12 <jalain_as@hotmail.com>
2023-10-03 13:18:45 -0600

    Segunda version

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,40 @@ const searchBarIsActive = () => navbarForm.classList.toggle('active');
 
 navbarSearchBtn.addEventListener('click', searchBarIsActive);
 navbarFormCloseBtn.addEventListener('click', searchBarIsActive);
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Selecciona los elementos del DOM
+  const genreSelect = document.getElementById("genre");
+  const yearSelect = document.getElementById("year");
+  const moviesGrid = document.querySelector(".movies-grid");
+
+  // Agrega un evento de cambio a los selectores
+  genreSelect.addEventListener("change", updateMovies);
+  yearSelect.addEventListener("change", updateMovies);
+
+  // Función para actualizar las películas según los filtros
+  function updateMovies() {
+    const selectedGenre = genreSelect.value;
+    const selectedYear = yearSelect.value;
+
+    // Itera sobre todas las tarjetas de películas y muestra/oculta según los filtros
+    const movieCards = moviesGrid.querySelectorAll(".movie-card");
+    movieCards.forEach((card) => {
+      const cardGenre = card.querySelector(".genre").innerText.toLowerCase();
+      const cardYear = card.querySelector(".year").innerText;
+
+      const genreMatch = selectedGenre === "all" || cardGenre === selectedGenre.toLowerCase();
+      const yearMatch = selectedYear === "all" || cardYear === selectedYear;
+
+      if (genreMatch && yearMatch) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  }
+});
+
+  
